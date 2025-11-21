52c61396f6778e702538a1ff0d4ad50adfd9ef6a
kukuhputra08 <149924715+kukuhputra08@users.noreply.github.com>
2025-11-21 19:51:08 +0700

    chore: remove all existing files

--- itsSkillsahre/admin/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "fp_its_skillshare";
-$conn = mysqli_connect($host, $user, $pass, $db);
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
